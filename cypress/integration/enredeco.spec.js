bb0f02798d21808f6a05f074a17c7f2cdde0be9e
Thyessa Goltara <thyessagoltara@outlook.com>
2023-04-12 10:16:42 -0300

    Alteracões endereço

--- cypress/integration/enredeco.spec.js
+++ cypress/integration/enredeco.spec.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import enderecoPage from "../support/page-objects/endereco.page";
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereço - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -9,7 +11,26 @@ describe('Funcionalidade Endereço - Faturamento e Entrega', () => {
         
     });
 
-    it('Deve fazer cadastro de faturamento válido', () => {
-                //cadastro de endereço
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
+        enderecoPage.editarEnderecoFaturamento('Alice', 'Oliveira', 'BV', 'Brasil', 'Vista do Limoeiro', '13', 'Vix', 'Espírito Santo', '29890000', '279998556', 'alice@hotmail.com')
+        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')
     });
+
+    it.only('Deve fazer cadastro de faturamento com sucesso - usando arquivo de dados', () => {
+        enderecoPage.editarEnderecoFaturamento(
+            dadosEndereco [1].nome,
+            dadosEndereco [1].Sobrenome,
+            dadosEndereco [1].empresa,
+            dadosEndereco [1].pais,
+            dadosEndereco [1].endereco,
+            dadosEndereco [1].numero,
+            dadosEndereco [1].cidade,
+            dadosEndereco [1].estado,
+            dadosEndereco [1].cep,
+            dadosEndereco [1].telefone,
+            dadosEndereco [1].email,
+            )
+        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')
+    });
+
 });
